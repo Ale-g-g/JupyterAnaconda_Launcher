605c363765128a73ffd019d552b3e3cb656d1b10
Alejandro García <alegarcia46@gmail.com>
2021-03-12 11:53:34 +0100

    Update JupyterAnaconda_Launcher.bat

--- JupyterAnaconda_Launcher.bat
+++ JupyterAnaconda_Launcher.bat
@@ -1,6 +1,6 @@
 @echo off
 
-:: Calling Anaconda (with activate.bat), it activates the base environment. Suggested path: <anaconda_dir>/Scripts/activate.bat <anaconda_dir>
+:: Calling Anaconda (with activate.bat), it activates the base environment. Suggested path: <anaconda_dir>/Scripts/activate.bat <anaconda_dir>:
 call C:\Users\[UserName]\anaconda3\Scripts\activate.bat C:\Users\[UserName]\anaconda3
 
 :: Selecting our desired environment:
